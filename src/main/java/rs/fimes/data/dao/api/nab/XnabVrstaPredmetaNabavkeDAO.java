d185200009f733277634e4c7ab2698cd8479c981
Milos Ilic  HP <milos.ilic@gmail.com>
2014-06-12 00:46:59 +0200

    XnabVrstaPredmetaNabavkeDAO

---
+++ src/main/java/rs/fimes/data/dao/api/nab/XnabVrstaPredmetaNabavkeDAO.java
@@ -0,0 +1,8 @@
+package rs.fimes.data.dao.api.nab;
+
+import rs.etf.rc.common.dao.api.BaseDaoApi;
+
+public interface XnabVrstaPredmetaNabavkeDAO extends
+        BaseDaoApi<rs.fimes.domain.nab.XnabVrstaPredmetaNabavke, Integer> {
+
+}
---
+++ src/main/java/rs/fimes/data/dao/impl/hibernate/nab/XnabVrstaPredmetaNabavkeDAOImpl.java
@@ -0,0 +1,34 @@
+package rs.fimes.data.dao.impl.hibernate.nab;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+
+import rs.etf.rc.common.application.Application;
+import rs.etf.rc.common.application.ComponentType;
+import rs.etf.rc.common.application.ConfigurationException;
+import rs.etf.rc.common.application.Module;
+import rs.fimes.data.dao.api.nab.XnabVrstaPredmetaNabavkeDAO;
+import rs.fimes.data.dao.generic.BaseDaoImplHibernate;
+import rs.fimes.data.dao.generic.ColumnAgregateProperty;
+import rs.fimes.data.dao.generic.OrderBy;
+import rs.fimes.data.dao.generic.QueryJoin;
+import rs.fimes.data.dao.generic.QueryRestriction;
+import rs.fimes.domain.nab.XnabVrstaPredmetaNabavke;
+
+public class XnabVrstaPredmetaNabavkeDAOImpl extends
+BaseDaoImplHibernate<XnabVrstaPredmetaNabavke, Integer> implements
+        XnabVrstaPredmetaNabavkeDAO {
+
+    public XnabVrstaPredmetaNabavkeDAOImpl(Module module, String daoId)
+            throws ConfigurationException {
+        super(module, daoId);
+        // TODO Auto-generated constructor stub
+    }
+
+    private static final long serialVersionUID = 7628644533614432926L;
+
+
+}
