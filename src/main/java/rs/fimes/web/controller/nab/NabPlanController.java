33670b91c1e03a717f6c2603e4470c121f8685b1
Milos Ilic  HP <milos.ilic@gmail.com>
2014-06-19 07:24:45 +0200

    @radi 20140619

--- src/main/java/rs/fimes/web/controller/nab/NabPlanController.java
+++ src/main/java/rs/fimes/web/controller/nab/NabPlanController.java
@@ -2,12 +2,16 @@ package rs.fimes.web.controller.nab;
 
 import rs.etf.rc.common.application.ConfigurationException;
 import rs.etf.rc.common.application.Module;
+import rs.fimes.domain.core.OrgFirma;
+import rs.fimes.service.api.nab.NabNaruciociServiceApi;
 import rs.fimes.service.api.nab.NabPlanServiceApi;
 import rs.fimes.web.controller.BaseController;
 
 public class NabPlanController extends BaseController{
 
     private NabPlanServiceApi nabPlanServiceApi;
+    private NabNaruciociServiceApi nabNaruciociServiceApi;
+    private OrgFirma orgFirma;
 
     private static final long serialVersionUID = -788600541631559492L;
 
@@ -17,9 +21,21 @@ public class NabPlanController extends BaseController{
         // TODO Auto-generated constructor stub
     }
     
+    public NabNaruciociServiceApi getNabNaruciociServiceApi() {
+        return nabNaruciociServiceApi;
+    }
+
+    public void setNabNaruciociServiceApi(
+            NabNaruciociServiceApi nabNaruciociServiceApi) {
+        this.nabNaruciociServiceApi = nabNaruciociServiceApi;
+    }
+
     public void onStart() {
         
         System.out.println( ":::Ukenjao sam se od srece NAbPlanController " + System.currentTimeMillis());
+        if ( orgFirma == null ) {
+            setOrgFirma(nabNaruciociServiceApi.getActiveOrgFirma(getUserSessionUtil().getCurrentUserCurrentOrgFirma().getIdFirma()));
+          }
              
     }
 
@@ -29,6 +45,14 @@ public class NabPlanController extends BaseController{
 
     public void setNabPlanServiceApi(NabPlanServiceApi nabPlanServiceApi) {
         this.nabPlanServiceApi = nabPlanServiceApi;
+    }
+
+    public OrgFirma getOrgFirma() {
+        return orgFirma;
+    }
+
+    public void setOrgFirma(OrgFirma orgFirma) {
+        this.orgFirma = orgFirma;
     }
 
 
