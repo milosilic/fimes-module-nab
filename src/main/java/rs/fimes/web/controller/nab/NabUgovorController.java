a9d2e0a7635009d22112ab7714e74410fcd8ff11
Milos Ilic  HP <milos.ilic@gmail.com>
2014-08-31 13:40:51 +0200

    nabUgovorController

---
+++ src/main/java/rs/fimes/web/controller/nab/NabUgovorController.java
@@ -0,0 +1,19 @@
+package rs.fimes.web.controller.nab;
+
+import rs.etf.rc.common.application.ConfigurationException;
+import rs.etf.rc.common.application.Module;
+import rs.fimes.web.controller.BaseController;
+
+public class NabUgovorController extends BaseController{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public NabUgovorController(Module module, String controllerId)
+            throws ConfigurationException {
+        super(module, controllerId);
+    }
+
+}
