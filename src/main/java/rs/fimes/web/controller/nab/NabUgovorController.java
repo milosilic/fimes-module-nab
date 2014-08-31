ef2dd95323c64ed9620bef209d698f3fea914e88
Milos Ilic  HP <milos.ilic@gmail.com>
2014-08-31 16:33:07 +0200

    UGOVOR nabUgovor.jspx  prikaz

--- src/main/java/rs/fimes/web/controller/nab/NabUgovorController.java
+++ src/main/java/rs/fimes/web/controller/nab/NabUgovorController.java
@@ -2,6 +2,7 @@ package rs.fimes.web.controller.nab;
 
 import rs.etf.rc.common.application.ConfigurationException;
 import rs.etf.rc.common.application.Module;
+import rs.fimes.domain.nab.NabUgovor;
 import rs.fimes.web.controller.BaseController;
 import rs.fimes.web.datamodel.api.nab.NabUgovorExtendedDataTableModelApi;
 
@@ -9,10 +10,19 @@ public class NabUgovorController extends BaseController{
 
     private static final long serialVersionUID = 1L;
     private NabUgovorExtendedDataTableModelApi nabUgovorExtendedDataTableModelApi;
+    
+    private String interniBroj;
+    private NabUgovor nabUgovorSelected;
 
     public NabUgovorController(Module module, String controllerId)
             throws ConfigurationException {
         super(module, controllerId);
+        System.out.println("*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/");
+    }
+    
+    // TODO implementirati  
+    public void initNoviUgovor(){
+        
     }
 
     public NabUgovorExtendedDataTableModelApi getNabUgovorExtendedDataTableModelApi() {
@@ -22,6 +32,22 @@ public class NabUgovorController extends BaseController{
     public void setNabUgovorExtendedDataTableModelApi(
             NabUgovorExtendedDataTableModelApi nabUgovorExtendedDataTableModelApi) {
         this.nabUgovorExtendedDataTableModelApi = nabUgovorExtendedDataTableModelApi;
+    }
+
+    public String getInterniBroj() {
+        return interniBroj;
+    }
+
+    public void setInterniBroj(String interniBroj) {
+        this.interniBroj = interniBroj;
+    }
+
+    public NabUgovor getNabUgovorSelected() {
+        return nabUgovorSelected;
+    }
+
+    public void setNabUgovorSelected(NabUgovor nabUgovorSelected) {
+        this.nabUgovorSelected = nabUgovorSelected;
     }
 
 }
